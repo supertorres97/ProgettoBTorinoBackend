0b783457768d561f4cb9dd7e3dbc26ef5eeadb7a
danielpaliuc <daniel.paliuc16@gmail.com>
2025-01-30 15:10:47 +0100

    PRIMO COMMIT

---
+++ PasticceriaBackend/src/main/java/com/betacom/pasticceria/PasticceriaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.betacom.pasticceria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasticceriaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasticceriaBackendApplication.class, args);
+	}
+
+}
---
+++ PasticceriaBackend/src/test/java/com/betacom/pasticceria/PasticceriaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betacom.pasticceria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasticceriaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
