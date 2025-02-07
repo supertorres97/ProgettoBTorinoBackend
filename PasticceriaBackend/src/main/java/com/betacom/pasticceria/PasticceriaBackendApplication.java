8e21af3b278aaf30b07cf811b6a0381316d4280d
MatteRinaldi <matte.rina15@gmail.com>
2025-02-07 15:34:00 +0100

    tolt bn

--- PasticceriaBackend/src/main/java/com/betacom/pasticceria/PasticceriaBackendApplication.java
+++ PasticceriaBackend/src/main/java/com/betacom/pasticceria/PasticceriaBackendApplication.java
@@ -1,12 +1,7 @@
 package com.betacom.pasticceria;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Scope;
 
 @SpringBootApplication
 public class PasticceriaBackendApplication {
@@ -15,9 +10,4 @@ public class PasticceriaBackendApplication {
 		SpringApplication.run(PasticceriaBackendApplication.class, args);
 	}
 
-	@Bean	
-	@Scope("prototype")		
-	Logger logger (InjectionPoint ip) {
-		return LoggerFactory.getLogger(ip.getMember().getDeclaringClass());
-	}
 }
