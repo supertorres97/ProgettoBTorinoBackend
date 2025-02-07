d3d9e547fcc7a17f97a99c942ac0a7f13f30b26f
super_torres97 <supertorres97@gmail.com>
2025-02-07 15:23:50 +0100

    aggiunti in pom.xml dependency junit e aggiunto annotazione suite e select classes

--- PasticceriaBackend/src/test/java/com/betacom/pasticceria/PasticceriaBackendApplicationTests.java
+++ PasticceriaBackend/src/test/java/com/betacom/pasticceria/PasticceriaBackendApplicationTests.java
@@ -1,8 +1,14 @@
 package com.betacom.pasticceria;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Suite
+@SelectClasses({
+	
+})
 @SpringBootTest
 class PasticceriaBackendApplicationTests {
 
