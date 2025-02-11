2468a8467c7a77fb8183787000ab2cb28d51fffb
super_torres97 <supertorres97@gmail.com>
2025-02-11 11:42:20 +0100

    fixato dettagli ordine

--- PasticceriaBackend/src/main/java/com/betacom/pasticceria/repositories/DettagliOrdineRepository.java
+++ PasticceriaBackend/src/main/java/com/betacom/pasticceria/repositories/DettagliOrdineRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.betacom.pasticceria.model.DettagliOrdine;
 
 public interface DettagliOrdineRepository extends JpaRepository<DettagliOrdine, Integer>{
-	Optional<DettagliOrdine> findByOrdine_Id(String idOrdine);
+	Optional<DettagliOrdine> findByOrdine_Id(Integer idOrdine);
 }
